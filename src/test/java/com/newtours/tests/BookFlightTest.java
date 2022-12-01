da082d1f92d155e9f260dcda1163b0de3eeec5ad
Madeline Cardinale <mcardinale@deloitte.com>
2022-12-01 13:28:38 -0500

    first commit

---
+++ src/test/java/com/newtours/tests/BookFlightTest.java
@@ -0,0 +1,57 @@
+package com.newtours.tests;
+
+import com.newtours.pages.*;
+import com.tests.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class BookFlightTest extends BaseTest {
+
+    private String noOfPassengers;
+    private String expectedPrice;
+
+    @BeforeTest
+    @Parameters({"noOfPassengers", "expectedPrice"})
+    public void setupParameters(String noOfPassengers, String expectedPrice) {
+        this.noOfPassengers = noOfPassengers;
+        this.expectedPrice = expectedPrice;
+    }
+
+    @Test
+    public void registrationPageTest() {
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        registrationPage.goTo();
+        registrationPage.enterUserDetails("selenium", "docker");
+        registrationPage.enterUserCredentials("selenium", "docker");
+        registrationPage.submit();
+    }
+
+    @Test(dependsOnMethods = "registrationPageTest")
+    public void registrationConfirmationPageTest() {
+        RegistrationConfirmationPage registrationConfirmationPage = new RegistrationConfirmationPage(driver);
+        registrationConfirmationPage.goToFlightDetailsPage();
+    }
+
+    @Test(dependsOnMethods = "registrationConfirmationPageTest")
+    public void flightDetailsPageTest() {
+        FlightDetailsPage flightDetailsPage = new FlightDetailsPage(driver);
+        flightDetailsPage.selectPassengers(noOfPassengers);
+        flightDetailsPage.goToFindFlightsPage();
+    }
+
+    @Test(dependsOnMethods = "flightDetailsPageTest")
+    public void findFlightPageTest() {
+        FindFlightPage findFlightPage = new FindFlightPage(driver);
+        findFlightPage.submitFindFlightPage();
+        findFlightPage.goToFlightConfirmationPage();
+    }
+
+    @Test(dependsOnMethods = "findFlightPageTest")
+    public void flightConfirmationPageTest() {
+        FlightConfirmationPage flightConfirmationPage = new FlightConfirmationPage(driver);
+        String actualPrice = flightConfirmationPage.getPrice();
+        Assert.assertEquals(actualPrice, expectedPrice);
+    }
+}
